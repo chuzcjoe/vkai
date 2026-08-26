a2c9340c32eef84cfd79c74aee4ee9c8d047a4d2
chuzcjoe <joechu0510@gmail.com>
2026-08-26 11:33:47 -0700

    add clang-format

--- vkai/include/Linear.h
+++ vkai/include/Linear.h
@@ -5,29 +5,27 @@
 namespace vkai {
 
 class Linear : public Layer {
-
-public:
-  Linear(core::vulkan::VulkanContext *context,
-         core::vulkan::VulkanBuffer &input, core::vulkan::VulkanBuffer &weights,
-         core::vulkan::VulkanBuffer &bias, core::vulkan::VulkanBuffer &output,
-         int input_size, int output_size, int batch_size = 1);
+ public:
+  Linear(core::vulkan::VulkanContext* context, core::vulkan::VulkanBuffer& input,
+         core::vulkan::VulkanBuffer& weights, core::vulkan::VulkanBuffer& bias,
+         core::vulkan::VulkanBuffer& output, int input_size, int output_size, int batch_size = 1);
 
   void Init() override;
 
-  void Execute(const VkCommandBuffer &command_buffer) override;
+  void Execute(const VkCommandBuffer& command_buffer) override;
 
-protected:
+ protected:
   std::vector<core::vulkan::BindingInfo> GetBindingInfo() const override;
 
-  const std::vector<uint32_t> &LoadShaderCode() const override;
+  const std::vector<uint32_t>& LoadShaderCode() const override;
 
   const std::string GetPipelineCache() const override;
 
-private:
-  core::vulkan::VulkanBuffer &input_buffer_;
-  core::vulkan::VulkanBuffer &weights_buffer_;
-  core::vulkan::VulkanBuffer &bias_buffer_;
-  core::vulkan::VulkanBuffer &output_buffer_;
+ private:
+  core::vulkan::VulkanBuffer& input_buffer_;
+  core::vulkan::VulkanBuffer& weights_buffer_;
+  core::vulkan::VulkanBuffer& bias_buffer_;
+  core::vulkan::VulkanBuffer& output_buffer_;
 
   core::vulkan::VulkanBuffer uniform_buffer_;
   struct UniformData {
@@ -37,4 +35,4 @@ private:
   } uniform_data_;
 };
 
-} // namespace vkai
+}  // namespace vkai
