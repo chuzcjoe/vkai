a502863a36085176401f53b36e01f58f78852152
chuzcjoe <joechu0510@gmail.com>
2026-09-15 10:06:07 -0700

    update core version

--- build.sh
+++ build.sh
@@ -9,7 +9,20 @@ cd build
 
 project_root="$(cd .. && pwd)"
 cmake_options=(-DCMAKE_BUILD_TYPE=Debug
-               -DPIPELINE_CACHE_DIR="$project_root")
+               -DPIPELINE_CACHE_DIR="$project_root"
+               -DENABLE_EXTERNAL=1
+               -DENABLE_VULKAN=1
+               -DENABLE_OPENCL=0
+               -DENABLE_MAT=0
+               -DENABLE_TIMER=0
+               -DENABLE_TRACE=0
+               -DENABLE_OPENGL=0
+               -DENABLE_METAL=0
+               -DENABLE_EGL=0
+               -DENABLE_IO=0
+               -DENABLE_THREADPOOL=0
+               -DENABLE_TESTS=0
+               -DENABLE_EXAMPLES=0)
 
 cmake "${cmake_options[@]}" ..
 make -j10
